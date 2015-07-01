f0b19aabaf24438108aa477a72ab7726fc36110b
Nick <handnf@gmail.com>
2015-07-01 03:34:25 -0400

    Fixed bug
    There was an issue with using implicit intents. This resolves the that issue.

--- WeatherBroadcasterUtils.java
+++ WeatherBroadcasterUtils.java
@@ -1,9 +1,13 @@
 package com.felkertech.n.weatherdelta.utils;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.util.Log;
+
+import java.util.List;
 
 /**
  * Created by N on 6/2/2015.
@@ -158,8 +162,46 @@ public class WeatherBroadcasterUtils {
      * without instantiating an object of this class.
      * @return Intent to request weather data
      */
-    public static Intent getWeatherRequest() {
+    public static Intent getWeatherRequest(Context context) {
         Intent i = new Intent(REQUEST_WEATHER_ACTION);
-        return i;
+        return createExplicitFromImplicitIntent(context, i);
+    }
+
+    /* FROM http://blog.android-develop.com/2014/10/android-l-api-21-javalangillegalargumen.html */
+    /***
+     * Android L (lollipop, API 21) introduced a new problem when trying to invoke implicit intent,
+     * "java.lang.IllegalArgumentException: Service Intent must be explicit"
+     *
+     * If you are using an implicit intent, and know only 1 target would answer this intent,
+     * This method will help you turn the implicit intent into the explicit form.
+     *
+     * Inspired from SO answer: http://stackoverflow.com/a/26318757/1446466
+     * @param context
+     * @param implicitIntent - The original implicit intent
+     * @return Explicit Intent created from the implicit original intent
+     */
+    public static Intent createExplicitFromImplicitIntent(Context context, Intent implicitIntent) {
+        // Retrieve all services that can match the given intent
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> resolveInfo = pm.queryIntentServices(implicitIntent, 0);
+
+        // Make sure only one match was found
+        if (resolveInfo == null || resolveInfo.size() != 1) {
+            return null;
+        }
+
+        // Get component info and create ComponentName
+        ResolveInfo serviceInfo = resolveInfo.get(0);
+        String packageName = serviceInfo.serviceInfo.packageName;
+        String className = serviceInfo.serviceInfo.name;
+        ComponentName component = new ComponentName(packageName, className);
+
+        // Create a new intent. Use the old one for extras and such reuse
+        Intent explicitIntent = new Intent(implicitIntent);
+
+        // Set the component to be explicit
+        explicitIntent.setComponent(component);
+
+        return explicitIntent;
     }
 }
